d84ae2475aca9c1d436031044ad099fcc63d156a
alimuamar11 <alimuamar11@gmail.com>
2021-12-03 14:08:15 +0700

    another api without controller -> post table

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -33,4 +34,48 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     // API route for logout user
     Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
+});
+
+
+//another api without controller
+Route::get('/posts', function () {
+    return Post::all();
+});
+Route::get('/posts/{post}', function (Post $post) {
+    return Post::find($post);
+});
+
+Route::post('/posts', function () {
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+
+    return Post::create([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+});
+
+Route::put('/posts/{post}', function (Post $post) {
+    request()->validate([
+        'title' => 'required',
+        'content' => 'required',
+    ]);
+
+    $success = $post->update([
+        'title' => request('title'),
+        'content' => request('content'),
+    ]);
+
+    return [
+        'success' => $success
+    ];
+});
+Route::delete('/posts/{id}', function (Post $id) {
+    $success = $id->delete();
+    //same id,
+    return [
+        'success' => $success,
+    ];
 });
